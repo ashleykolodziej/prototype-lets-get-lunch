4be660342491e5f21970aa213be831d548811b03
Ashley Kolodziej <likeapenguininthedark@gmail.com>
2022-05-18 09:47:48 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"extends": "airbnb-base",
+	"rules": {
+		"linebreak-style": 0,
+		"indent": ["error", "tab"],
+		"no-tabs": ["error", { allowIndentationTabs: true }]
+	},
+	"env": {
+		"browser": true,
+		"node": true
+	}
+};
