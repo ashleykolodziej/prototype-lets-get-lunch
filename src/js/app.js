48879949b733a054b0dba6f8d718ecfc5a50da50
ashleykolodziej <kolodz10@bu.edu>
2022-05-18 22:46:34 -0400

    Working on framework now

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+import $ from 'dom7';
+import Framework7 from 'framework7/bundle';
+
+// Import F7 Styles
+import 'framework7/css/bundle';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.scss';
+
+
+// Import Routes
+import routes from './routes.js';
+// Import Store
+import store from './store.js';
+
+// Import Store
+import pickit from './pickit.js';
+
+// Import main app component
+import App from '../app.f7';
+
+
+const app = new Framework7({
+  name: 'Lets Get Lunch!', // App name
+  theme: 'ios', // Automatic theme detection
+  el: '#app', // App root element
+  component: App, // App main component
+
+  // App store
+  store: store,
+  // App routes
+  routes: routes
+});
+
+app.on('pageInit', pickit)
