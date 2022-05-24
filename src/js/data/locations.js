8b839cd76762907c204c978c77686dcbfe823c2e
ashleykolodziej <kolodz10@bu.edu>
2022-05-24 18:42:07 -0400

    Data and adjust labeling

--- src/js/data/locations.js
+++ src/js/data/locations.js
@@ -1,6 +1,11 @@
 import tacoPartyImg from '../../assets/2021-08-02.jpg'
 import tastyMoMoImg from '../../assets/2018-07-21.jpg'
 import mintImg from '../../assets/index.jpg'
+import soundBitesImg from '../../assets/about-sound-bites-cafe.jpg'
+import kebabImg from '../../assets/ab-kebab.jpg'
+import catalinasImg from '../../assets/cat-index.jpg'
+import sushiImg from '../../assets/sushi.jpg'
+import jumbosImg from '../../assets/jumbos.jpg'
 
 const locations = [
 	{
@@ -10,6 +15,10 @@ const locations = [
 			"pickup_menu_url": "data/menu-100489.json",
 			"image_url": tacoPartyImg,
 			"merchant_description": "Taco truck with vegan and gluten-free options.",
+			"street_address": "711 Broadway",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02144-1905",
 		}
 	},
 	{
@@ -17,8 +26,12 @@ const locations = [
 			"id": "779661",
 			"name": "Tasty Mo:Mo",
 			"pickup_menu_url": "data/menu-779661.json",
-			"image_url": tacoPartyImg,
+			"image_url": tastyMoMoImg,
 			"merchant_description": "Quick-serve, charity-minded eatery dishing out dumplings with spicy sauces & thali platters.",
+			"street_address": "508 Medford St",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02145-2656",
 		}
 	},
 	{
@@ -26,7 +39,11 @@ const locations = [
 			"id": "100490",
 			"name": "Eat At Jumbo's",
 			"pickup_menu_url": "data/menu-100490.json",
-			"image_url": null,
+			"image_url": jumbosImg,
+			"street_address": "688 Broadway",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02144-1905",
 		}
 	},
 	{
@@ -35,6 +52,10 @@ const locations = [
 			"name": "Mint Indian Eatery",
 			"pickup_menu_url": "data/menu-728388.json",
 			"image_url": mintImg,
+			"street_address": "868 Broadway",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02144-1905",
 		}
 	},
 	{
@@ -42,7 +63,11 @@ const locations = [
 			"id": "755898",
 			"name": "Casa Pizza",
 			"pickup_menu_url": "data/menu-755898.json",
-			"image_url": null,
+			"image_url": catalinasImg,
+			"street_address": "4267 Broadway",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02144-1905",
 		}
 	},
 	{
@@ -50,7 +75,11 @@ const locations = [
 			"id": "779266",
 			"name": "Ocean Sushi Restaurant",
 			"pickup_menu_url": "data/menu-779266.json",
-			"image_url": null,
+			"image_url": sushiImg,
+			"street_address": "902 Broadway",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02144-1905",
 		}
 	},
 	{
@@ -58,7 +87,11 @@ const locations = [
 			"id": "799571",
 			"name": "Catalina's Pizza",
 			"pickup_menu_url": "data/menu-799571.json",
-			"image_url": null,
+			"image_url": catalinasImg,
+			"street_address": "298 Broadway",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02144-1905",
 		}
 	},
 	{
@@ -66,7 +99,11 @@ const locations = [
 			"id": "779588",
 			"name": "The House of Kebab",
 			"pickup_menu_url": "data/menu-779588.json",
-			"image_url": null,
+			"image_url": kebabImg,
+			"street_address": "1003 Broadway",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02144-1905",
 		}
 	},
 	{
@@ -74,7 +111,11 @@ const locations = [
 			"id": "801423",
 			"name": "Sound Bites Cafe",
 			"pickup_menu_url": "data/menu-801423.json",
-			"image_url": null,
+			"image_url": soundBitesImg,
+			"street_address": "2967 Broadway",
+			"locality": "Somerville",
+			"region": "MA",
+			"postal_code": "02144-1905",
 		}
 	}
 ]
