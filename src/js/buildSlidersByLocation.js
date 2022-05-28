dd91bf73283bfe9577618d9f1ad740f3e20b9d9a
ashleykolodziej <kolodz10@bu.edu>
2022-05-28 11:39:30 -0400

    Group items by location

---
+++ src/js/buildSlidersByLocation.js
@@ -0,0 +1,110 @@
+import { checkVeg, checkVegan } from '../js/checkdishes';
+
+function isEmpty(obj) {
+	return Object.keys(obj).length === 0;
+}
+
+async function buildSlidersByLocation(data, $f7) {
+	const sliderMarkup = document.createElement('div');
+
+	Object.values(data).forEach(item => {
+		//item.categories.forEach(item => {
+			const restaurant = item.restaurantName;
+			const category = item.name;
+
+			if (category === 'Beverages') {
+				return;
+			}
+
+			const title = document.createElement('div');
+			const classes = ['block-title', 'block-title-medium'];
+					title.textContent = restaurant;
+					title.classList.add(...classes);
+
+			const swiperTemplate = document.createElement('div');
+		   		swiperTemplate.classList.add('swiper');
+		   const swiperWrapper = document.createElement('div');
+		   		swiperWrapper.classList.add('swiper-wrapper');
+		   		swiperTemplate.appendChild(swiperWrapper);
+
+			const swiper = $f7.swiper.create(swiperTemplate, {
+				// Optional parameters
+				direction: 'horizontal',
+				loop: false,
+				effect: "cards"
+			});
+
+			let slideCount = 0;
+
+			for ( const dish of item.items ) {
+				const img = dish.item.image_url,
+						name = dish.item.name,
+						description = dish.item.description,
+						allOfIt = name + ' ' + description;
+				if (
+					null !== img &&
+					null !== name &&
+					null !== description &&
+					'' !== description
+				) {
+					// Crummy vegan filter test
+					const isVeg = checkVeg(allOfIt);
+					let isVegan = false;
+
+					if (isVeg) {
+						isVegan = checkVegan(allOfIt);
+					}
+
+					// Something about this isn't working right now.
+					// It's only returning one all veg menu.
+					if (!isVeg) {
+						return;
+					}
+
+					slideCount++;
+
+					swiper.appendSlide(`<div
+							data-restaruant-id="${dish.item.restaurantId}"
+							data-dish-id="${dish.item.id}"
+							data-dish-name="${name}"
+							data-dish-location="${dish.item.restaurantName}"
+							data-dish-description="${description}"
+							data-dish-img="${img}?density=1&height=300&width=420"
+							class="swiper-slide">
+							<div class="card">
+							<div class="card-media">
+								<img src="${img}?density=1&height=300&width=420">
+							</div>
+							<div class="card-content card-content-padding">
+							<div class="card-meta">
+								<div class="tags">
+									${ isVeg ? '<span class="badge">vegetarian</span>' : '' }
+									${ isVegan ? '<span class="badge">vegan</span>' : '' }
+								</div>
+								<div class="card-favorite">
+									Like
+									<i class="icon f7-icons if-not-md">heart</i>
+									<i class="icon material-icons if-md">heart</i>
+								</div>
+							</div>
+							<h3>${name}</h3>
+							<p>${description}</p>
+							</div>
+							<div class="card-footer">in ${dish.item.categoryName}</p>
+							</div>
+						</div>`);
+					}
+  			}
+
+  			if (slideCount > 0) {
+  				title.textContent += ` (${slideCount})`;
+  				sliderMarkup.appendChild(title);
+  				sliderMarkup.appendChild(swiper.el);
+  			}
+			});
+		//});
+
+	return sliderMarkup;
+}
+
+export default buildSlidersByLocation;
--- src/js/getAllDishesByLocation.js
+++ src/js/getAllDishesByLocation.js
@@ -2,37 +2,45 @@ import getObjectKeyByKeyValuePair from './utilities.js';
 import locations from './data/locations';
 import getMenuData from './getMenuData';
 
-async function getAllDishesByLocation( locationArr = locations ) {
-	let dishesByCategory = [];
+async function getAllDishesByLocation( locationArr = locations, byCategory = false ) {
+	let dishesArray = [];
 
 	for (let location of locationArr) {
 		await getMenuData(location.location.pickup_menu_url).then(data => {
 			data.menu.categories.map((category) => {
+				category.category.restaurantId = location.location.id;
+				category.category.restaurantName = location.location.name;
 				// Add an association point between items and restaurants
 				category.category.items.map((item) => {
+					item.item.categoryName = category.category.name;
 					item.item.restaurantName = location.location.name;
 					item.item.restaurantId = location.location.id;
-				})
+				});
 
-				const categoryKey = getObjectKeyByKeyValuePair(dishesByCategory, 'name', category.category.name);
+				let matchingKey;
 
-				// TODO: This will combine _exact_ same categories, but I should find a way to combine
-				// like categories as well. Example: Beverages and Drinks should probably be the same
-				// category in this map.
-				// Perhaps mapping categories by cuisine or item type could be good.
+				if ( byCategory ) {
+					// TODO: This will match _exact_ same categories, but I should find a way to combine
+					// like categories as well. Example: Beverages and Drinks should probably be the same
+					// category in this map.
+					// Perhaps mapping categories by cuisine or item type could be good.
+					matchingKey = getObjectKeyByKeyValuePair(dishesArray, 'name', category.category.name);
+				} else {
+					matchingKey = getObjectKeyByKeyValuePair(dishesArray, 'restaurantId', category.category.restaurantId);
+				}
 
-				if ( undefined === categoryKey ) {
+				if ( undefined === matchingKey ) {
 					// Add the new category to the list
-					dishesByCategory.push(category.category);
+					dishesArray.push(category.category);
 				} else {
 					// Combine this restaurant's dishes into this category
-					dishesByCategory[categoryKey].items = [...dishesByCategory[categoryKey].items, ...category.category.items]
+					dishesArray[matchingKey].items = [...dishesArray[matchingKey].items, ...category.category.items]
 				}
 			});
 		});
 	}
 
-	return dishesByCategory;
+	return dishesArray;
 }
 
 export default getAllDishesByLocation;
